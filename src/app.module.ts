05ce613c2e28aff1d4dd96644154e2ba6c4767c9
Mekkit <hmek1987@gmail.com>
2024-10-23 22:48:06 +0700

    add DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ 
+    ItemsModule,  
+    ConfigModule.forRoot({ isGlobal: true }), 
+    DbModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
